cf1f23d863f885a33597acc24e67c76c9e5f1aab
Alex Chu <chu.alex9102@gmail.com>
2020-02-03 12:54:01 -0500

    finished database code to dynamically update to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,82 @@
 // Branden has firebase control
 var firebaseConfig = {
-  apiKey: firebase.apiKey,
-  authDomain: "employee-scheduler-402b9.firebaseapp.com",
-  databaseURL: "https://employee-scheduler-402b9.firebaseio.com",
-  projectId: "employee-scheduler-402b9",
-  storageBucket: "employee-scheduler-402b9.appspot.com",
-  messagingSenderId: "336373202513",
-  appId: "1:336373202513:web:d24ebf56b4a074ffd74270"
+	apiKey            : firebase.apiKey,
+	authDomain        : 'employee-scheduler-402b9.firebaseapp.com',
+	databaseURL       : 'https://employee-scheduler-402b9.firebaseio.com',
+	projectId         : 'employee-scheduler-402b9',
+	storageBucket     : 'employee-scheduler-402b9.appspot.com',
+	messagingSenderId : '336373202513',
+	appId             : '1:336373202513:web:d24ebf56b4a074ffd74270'
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
-var employee = {
-  name: "Branden Patten",
-  phoneNumber: "(239)-699-2087",
-  email: "patte.branden@gmail.com",
-  age: 26,
-  roles: ["baker", "bakery clerk", "cake decorator", "cashier"],
-  account: {
-  username: "username",
-  password: "badplaintextpassword"}
-}
+$('#add-employee').on('click', function (event) {
+	// Prevent the default form submit behavior
+	event.preventDefault();
+	// Grabs user input
+	var employeeName = $('#employee-name').val().trim();
+	var role = $('#role').val().trim();
+	var phone = $('#phone-number-form').val().trim();
+	var email = $('#email-form').val().trim();
 
-function addUser() {
-  var timestamp = firebase.database.ServerValue.TIMESTAMP
-  database.ref().child('employee').push({
+	var newEmployee = {
+		employeeName : employeeName,
+		role         : role,
+		phone        : phone,
+		email        : email
+	};
 
-    fullName: employee.name,
-    email: employee.email,
-    age: employee.age,
-    phoneNumber: employee.phoneNumber,
-    roles: employee.roles,
-    username: employee.account.username,
-    password: employee.account.password,
-    dateAdded: timestamp
-  })
+	database.ref().push(newEmployee);
 
+	$('#employee-name').val('');
+	$('#role').val('');
+	$('#phone-number-form').val('');
+	$('#email-form').val('');
+});
 
-  // database.ref("/login").push({
-  //   dateAdded: timestamp
-  // })
-}
-addUser()
+database.ref().on('child_added', function (childSnapshot, prevChildKey) {
+	console.log(childSnapshot.val());
 
+	// Store everything into a variable.
+	var eName = childSnapshot.val().employeeName;
+	var eRole = childSnapshot.val().role;
+	var ePhone = childSnapshot.val().phone;
+	var eEmail = childSnapshot.val().email;
 
+	$('#employee-table > tbody').append(
+		$('<tr>').append($('<td>').text(eName), $('<td>').text(eRole), $('<td>').text(ePhone), $('<td>').text(eEmail))
+	);
+});
+
+// var employee = {
+// 	name        : 'Branden Patten',
+// 	phoneNumber : '(239)-699-2087',
+// 	email       : 'patte.branden@gmail.com',
+// 	age         : 26,
+// 	roles       : [ 'baker', 'bakery clerk', 'cake decorator', 'cashier' ],
+// 	account     : {
+// 		username : 'username',
+// 		password : 'badplaintextpassword'
+// 	}
+// };
+
+// function addUser () {
+// 	var timestamp = firebase.database.ServerValue.TIMESTAMP;
+// 	database.ref().child('employee').push({
+// 		fullName    : employee.name,
+// 		email       : employee.email,
+// 		age         : employee.age,
+// 		phoneNumber : employee.phoneNumber,
+// 		roles       : employee.roles,
+// 		username    : employee.account.username,
+// 		password    : employee.account.password,
+// 		dateAdded   : timestamp
+// 	});
+
+// 	// database.ref("/login").push({
+// 	//   dateAdded: timestamp
+// 	// })
+// }
+// addUser();
---
+++ assets/javascript/config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+	apiKey            : 'AIzaSyByu_7sv8S3JRoqjQ8wApeCj89z5wmvZDg',
+	authDomain        : 'employee-scheduler-402b9.firebaseapp.com',
+	databaseURL       : 'https://employee-scheduler-402b9.firebaseio.com',
+	projectId         : 'employee-scheduler-402b9',
+	storageBucket     : 'employee-scheduler-402b9.appspot.com',
+	messagingSenderId : '336373202513',
+	appId             : '1:336373202513:web:d24ebf56b4a074ffd74270'
+};
