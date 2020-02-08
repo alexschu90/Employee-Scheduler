be3af20e04a8f7a4ba6cec975b8a71d7233f86fa
pattenbranden <patten.branden@gmail.com>
2020-02-08 10:21:39 -0500

    adding highres header image. added google fonts. modified alert text on invalid form entries

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -13,7 +13,6 @@ firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
 
-
 $('#submit-employee').on('click', function (event) {
 	// Prevent the default form submit behavior
 	event.preventDefault();
@@ -22,6 +21,8 @@ $('#submit-employee').on('click', function (event) {
 	var role = $('#role').val().trim();
 	var phone = $('#phone-number-form').val().trim();
 	var email = $('#email-form').val().trim();
+	var emWarnStatus = false
+	var phWarnStatus = false
 
 
 	// This function checks the phone field and if it's a valid phone-number, returns true. 
@@ -38,19 +39,29 @@ $('#submit-employee').on('click', function (event) {
 		console.log("is this a valid email? " + emailPattern.test(email))
 		return emailPattern.test(email);
 	};
+	// this adds the warning text if our phonenumber is invalid 
 	if (validPhoneNumber(phone) === false) {
-		badPhWarn = $("<p id = 'ph-warn' style='color:#ff0000'>Please enter a valid phone-number!</p>")
-		$("#ph-input").append(badPhWarn)
+		if (phWarnStatus === false) {
+			badPhWarn = $("<p id = 'ph-warn' style='color:#ff0000'>Please enter a valid phone-number!</p>")
+			$("#ph-input").append(badPhWarn)
+			phWarnStatus = true
+		}
 	} else {
 		$("#ph-warn").empty()
+		phWarnStatus = false
 	}
+	// this adds the warning text if our email is invalid 
 	if (validEmailAdress(email) === false) {
+		if (emWarnStatus === false) {
 		badEmWarn = $("<p id = 'em-warn' style='color:#ff0000'>Please enter a valid email address!</p>")
 		$("#em-input").append(badEmWarn)
+		emWarnStatus = true
+		}
 	} else {
 		$("#em-warn").empty()
+		emWarnStatus = false
 	}
-
+	// if we have both a valid phone number AND a valid email address, post fields to firebase as one object and clear the fields 
 	if (validPhoneNumber(phone) === true && validEmailAdress(email) === true) {
 
 		var newEmployee = {
@@ -72,7 +83,6 @@ $('#submit-employee').on('click', function (event) {
 
 });
 database.ref().on('child_added', function (childSnapshot, prevChildKey) {
-	console.log(childSnapshot.val());
 
 	// Store everything into a variable.
 	var eName = childSnapshot.val().employeeName;
