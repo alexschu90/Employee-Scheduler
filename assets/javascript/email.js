cc3d6e508ff10084a5d39fe451c4350d9197c63e
slayeroak <roakley1@villanova.edu>
2020-02-08 11:23:03 -0500

    fixed email domain checker

--- assets/javascript/email.js
+++ assets/javascript/email.js
@@ -1,16 +1,16 @@
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://pozzad-email-validator.p.rapidapi.com/emailvalidator/validateEmail/john%2540gmail.com",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "pozzad-email-validator.p.rapidapi.com",
-		"x-rapidapi-key": "c388bad2ddmshe28e07e5b16dc28p1a0c5djsnafe9a65d329c"
-	}
-}
+// var settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://pozzad-email-validator.p.rapidapi.com/emailvalidator/validateEmail/john%2540gmail.com",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "pozzad-email-validator.p.rapidapi.com",
+// 		"x-rapidapi-key": "c388bad2ddmshe28e07e5b16dc28p1a0c5djsnafe9a65d329c"
+// 	}
+// }
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
-    var res = response;
-    console.log(res);
-});
+// $.ajax(settings).done(function (response) {
+//     console.log(response);
+//     var res = response;
+//     console.log(res);
+// });
